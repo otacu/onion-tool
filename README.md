2409719ca74dcfc775d2005dc12084efe707aced
JingshengYe <jingsheng-ye@msyc.cc>
2019-11-11 18:09:45 +0800

    :wrench: 删除第三方日志搜索功能

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 
 ####1、关联查询工具
 导入一个excel，根据sheet1的数据和sheet2的sql模板拼成sql查询（数据标题就是模板参数名，模板参数用{{模板参数名}}来识别），返回结果用excel导出。
-####2、第三方日志elasticsearch索引
-用Spring Data Elasticsearch将第三方日志放进es索引，提高对报文内容的全文搜索速度。
--- src/main/java/com/egoist/oniontool/dao/elasticsearch/ThirdPlatformLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.egoist.oniontool.dao.elasticsearch;
-
-import com.egoist.oniontool.pojo.elasticsearch.ThridPlatformLogDetailDoc;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ThirdPlatformLogRepository extends ElasticsearchRepository<ThridPlatformLogDetailDoc, Long> {
-}
