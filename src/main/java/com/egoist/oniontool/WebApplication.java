8ef8cb606da573c1cba1b7468760c9c415cfbd88
JingshengYe <jingsheng-ye@msyc.cc>
2019-11-11 18:19:23 +0800

    :wrench: 删除第三方日志搜索功能

--- src/main/java/com/egoist/oniontool/WebApplication.java
+++ src/main/java/com/egoist/oniontool/WebApplication.java
@@ -3,10 +3,8 @@ package com.egoist.oniontool;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
-@EnableElasticsearchRepositories(basePackages = "com.egoist.oniontool.dao.elasticsearch")
 @MapperScan("com.egoist.oniontool.dao")//将项目中对应的mapper类的路径加进来就可以了
 public class WebApplication {
 
